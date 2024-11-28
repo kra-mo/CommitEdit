e521b557cb828836f66539edb45b4c20ab9a6151
kramo <contact@kramo.page>
2024-11-28 21:06:18 +0100

    Add a pointless but fun animation

--- CommitEdit/WelcomeView.swift
+++ CommitEdit/WelcomeView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct WelcomeView: View {
     @Environment(\.openURL) private var openURL
 
-    @State private var closeButtonOpacity = 0.5
     @State private var donateAlertPresented = false
+
+    @State private var closeButtonOpacity = 0.5
+
+    @State private var scaleCopyButton = false
+    @State private var showCopiedLabel = false
+    @State private var scaleCopiedLabel = false
+
 
     let gitCommand = "git config --global core.editor \"open -W -a 'CommitEdit'\""
 
@@ -25,25 +31,71 @@ struct WelcomeView: View {
                             .frame(width: 150, height: 150)
                             .shadow(color: Color.blue.opacity(0.5), radius: 60)
                     }
+
                     Text("CommitEdit").font(.largeTitle)
                         .fontWeight(.bold).padding()
+
                     Text("Run the following in the Terminal:")
                         .font(.title2)
+
                     Text(gitCommand)
                         .font(.system(.headline, design: .monospaced))
                         .padding(5)
                         .background(Color.black.opacity(0.5))
                         .foregroundColor(Color.white)
                         .cornerRadius(5)
-                    Button(
-                        "Copy Command",
-                        systemImage: "document.on.document"
-                    ) {
+
+                    Button {
                         NSPasteboard.general.clearContents()
                         NSPasteboard.general.setString(gitCommand, forType: .string)
+
+                        struct Keyframe {
+                            let duration: TimeInterval
+                            let action: () -> Void
+                        }
+
+                        var delay: TimeInterval = 0.00
+                        for key in [
+                            Keyframe(duration: 0.05, action: {
+                                scaleCopyButton = true;
+                                scaleCopiedLabel = true;
+                            }),
+                            Keyframe(duration: 0.05, action: { showCopiedLabel = true; }),
+                            Keyframe(duration: 1.00, action: {
+                                scaleCopyButton = false;
+                                scaleCopiedLabel = false;
+                            }),
+                            Keyframe(duration: 0.10, action: { scaleCopiedLabel = true; }),
+                            Keyframe(duration: 0.10, action: { showCopiedLabel = false; }),
+                            Keyframe(duration: 0.00, action: { scaleCopiedLabel = false; }),
+                        ] {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                                withAnimation { key.action() }
+                            }
+
+                            delay += key.duration
+                        }
+                    } label: {
+                        VStack {
+                            if showCopiedLabel {
+                                Label(
+                                    "Copied",
+                                    systemImage: "checkmark"
+                                ).scaleEffect(scaleCopiedLabel ? 0.8 : 1)
+                            } else {
+                                Label(
+                                    "Copy Command",
+                                    systemImage: "document.on.document.fill"
+                                ).scaleEffect(scaleCopiedLabel ? 0.8 : 1)
+                            }
+                        }
+                        .frame(minWidth: 150)
+                        .opacity(0.8)
                     }
                     .controlSize(.large)
+                    .scaleEffect(scaleCopyButton ? 0.9 : 1)
                     .padding()
+
                     Text("After that, commit messages should open for editing here.")
                         .padding()
                 }.padding()
